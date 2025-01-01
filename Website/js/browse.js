c765669496bcbe54f393ee12206c1ca4bd715bf7
Musa <good.24165@gmail.com>
2025-01-01 11:02:50 +0300

    add wrapper in browse

--- Website/js/browse.js
+++ Website/js/browse.js
@@ -1,3 +1,33 @@
-function Click(){
-    let
+var next = 257;
+var pre = -257;
+var index = ["first", "second", "third"];
+var i = 0;
+var j = 0;
+
+function Next(){
+    var block = document.getElementById("games-container");
+    block.style.transform = `translateX(${next}px)`;
+    var block1 = document.getElementById(index[j % 3]);
+    var parent = document.getElementById("games-container");
+    j += 1;
+    setTimeout(function() {
+        block.style.transition = "none";
+        block.style.transform = `translateX(0px)`;
+        parent.insertBefore(block1, parent.firstChild);
+    }, 500);
+    block.style.transition = "transform 0.5s ease-in-out";
+}
+
+function Pre(){
+    var block = document.getElementById("games-container");
+    block.style.transform = `translateX(${pre}px)`;
+    var block1 = document.getElementById(index[i % 3]);
+    i += 1;
+    var parent = document.getElementById("games-container");
+    setTimeout(function() {
+        block.style.transition = "none";
+        block.style.transform = `translateX(0px)`;
+        parent.appendChild(block1);
+    }, 500);
+    block.style.transition = "transform 0.5s ease-in-out";
 }
